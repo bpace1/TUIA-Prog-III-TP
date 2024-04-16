17a7d2bce1e23e9c9a95a9eee66f4d463dc13190
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-04-16 17:29:29 -0300

    DFS implemmented and working.

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -3,7 +3,6 @@ from ..models.frontier import StackFrontier, QueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
-
 class DepthFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
@@ -16,10 +15,10 @@ class DepthFirstSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node: Node = Node("", grid.start, 0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
+        explored: dict = {} 
         
         # Add the node to the explored dictionary
         explored[node.state] = True
@@ -27,7 +26,30 @@ class DepthFirstSearch:
         frontier: QueueFrontier  = QueueFrontier()
         frontier.add(node)
         
-        while True:
-            
+        if node.state == grid.end:
+            return Solution(node, explored)
         
+        while True:
+            if frontier.is_empty(): return NoSolution(explored)
+            temp_node: Node = frontier.remove()
+            
+            if temp_node.state in explored.keys() and temp_node.state != node.state: continue
+            explored[temp_node.state] = True
+            
+            actions: dict[str, tuple[int, int]] = grid.get_neighbours(temp_node.state)
+            
+            for action in actions:
+                solution: tuple[int, int] = actions[action]
+
+                if solution not in explored.keys(): 
+                    new_node: Node = Node(value="",
+                                          state=solution,
+                                          cost=temp_node.cost+grid.get_cost(solution),
+                                          parent=temp_node,
+                                          action=action)
+                                 
+                    if grid.end == new_node.state: return Solution(new_node, explored)
+                    frontier.add(new_node)       
+                    
+                
         return NoSolution(explored)
