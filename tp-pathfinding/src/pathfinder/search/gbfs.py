06f22cce202c8be1fd33e8762d3c534c9209a01f
bpace1 <82056912+bpace1@users.noreply.github.com>
2024-04-29 18:01:00 -0300

    GBFS now working

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,25 +3,57 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def manhattan_distance(state: tuple[int, int],
+                       goal: tuple[int, int]) -> int:
+    """
+    Calculate the Manhattan distance heuristic between the current state and a fixed goal state.
+    """
+    return abs(state[0] - goal[0]) + abs(state[1] - goal[1])
 
 class GreedyBestFirstSearch:
     @staticmethod
-    def search(grid: Grid) -> Solution:
-        """Find path between two points in a grid using Greedy Best First Search
+    def search(grid: Grid) -> Solution | NoSolution:
+            """Find path between two points in a grid using Greedy 
+             First  Search
 
-        Args:
-            grid (Grid): Grid of points
+            Args:
+                grid (Grid): Grid of points
 
-        Returns:
-            Solution: Solution found
-        """
-        # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+            Returns:
+                Solution: Solution found
+            """
+            # Initialize a node with the initial position
+            node: Node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+            frontier: PriorityQueueFrontier = PriorityQueueFrontier()
+            
+            frontier.add(node, manhattan_distance(node.state, grid.end))
+            
+            # Initialize the explored dictionary to be empty
+            explored: dict = {} 
+            
+            # Add the node to the explored dictionary
+            explored[node.state] = node
+            
+            while True:
+                if frontier.is_empty(): return NoSolution
+
+                new_node: Node = frontier.pop()
+                
+                if grid.end == new_node.state: return Solution(new_node, explored)
+
+                actions: dict[str, tuple[int, int]] = grid.get_neighbours(new_node.state)
+
+                for action in actions:
+                    child: Node = Node(value="",
+                                    state=actions[action],
+                                    cost=new_node.cost+grid.get_cost(actions[action]),
+                                    parent=new_node,
+                                    action=action)
+                    if child.state not in explored.keys() or child.cost < explored[child.state].cost:
+                        explored[child.state] = child
+                        frontier.add(child, manhattan_distance(child.state,  grid.end))
+            
+            return NoSolution(explored)
+
+
